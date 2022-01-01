6e96dac91f600a4535fae64f82965ecbf7668cd9
hi <69400390+fortgayhub@users.noreply.github.com>
2022-01-01 14:34:25 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,6 @@
 if game.PlaceId == 5490351219 then
    loadstring(game:HttpGet('https://raw.githubusercontent.com/fortgayhub/Kom-Hub/main/ClickerMadness.lua')()
+elseif game.PlaceId == 1962086868 then
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/fortgayhub/Kom-Hub/main/Tower%20of%20Hell.lua')()
+   end
 end
